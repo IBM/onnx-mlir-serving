24a5fe77f94277de939c035174f3de7c3290af26
Qin Yue Chen <chenqiny@cn.ibm.com>
2022-06-14 14:13:17 +0800

    update grpc-test script and position

--- tests/grpc-test.cc
+++ tests/grpc-test.cc
@@ -9,7 +9,7 @@ namespace {
 
 TEST(mnist, 1) {
 
-  Dataset ds("/workdir/aigrpc-server/tests/inputs_mnist");
+  Dataset ds("./models/mnist");
   InferenceClient client(grpc::CreateChannel("localhost:50051", grpc::InsecureChannelCredentials()));
   std::vector<float> out = client.Inference(&ds, 0);
   std::cout << out[0] << std::endl;
